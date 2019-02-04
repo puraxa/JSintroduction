c61bdefb99392a69abc3704b9ae4780ca6b89092
Edin Purivatra <edinpurivatra@gmail.com>
2019-02-04 20:58:33 +0100

    concat strings added

---
+++ concatstrings.js
@@ -0,0 +1,10 @@
+let name = 'Edin';
+let surname = 'Purivatra';
+let wordOne = 'broj';
+let wordTwo = 'telefona';
+let phoneNumber = '061/522-240';
+let space = ' ';
+
+let sentance = name + space + surname + space + wordOne + space + wordTwo + space + phoneNumber;
+
+console.log(sentance);
